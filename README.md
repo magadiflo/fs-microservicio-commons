c6fa0e5c18207b5ed5bd9bc93cfe9b89c3a0bec5
Martín <magadiflo@gmail.com>
2022-11-10 17:14:24 -0500

    Proyecto base - Con dependencias base: Spring Data JPA y Spring Web

---
+++ README.md
@@ -0,0 +1,5 @@
+# Microservicios Spring Cloud Eureka & Angular Full-stack
+
+## Sección 3: Backend: Eureka Server registrando microservicios
+
+### 21. Añadiendo librería commons para un service genérico y reutilizar código
---
+++ src/main/java/com/magadiflo/commons/FsMicroservicioCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsMicroservicioCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsMicroservicioCommonsApplication.class, args);
+	}
+
+}
