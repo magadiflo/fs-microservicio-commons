4e3614331b7bf0c9946e1531bd6b298174c0e6e8
Martín <magadiflo@gmail.com>
2022-11-26 23:23:58 -0500

    Permitiendo peticiones desde otro dominio con @CrossOrigin (Forma 01)
    Implementando la vista html para listar los alumnos

--- src/main/java/com/magadiflo/commons/controllers/CommonController.java
+++ src/main/java/com/magadiflo/commons/controllers/CommonController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +20,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.magadiflo.commons.services.ICommonService;
 
 /**
- * El editar no lo colocamos ya que cada Entity tiene una manera de actualizar
- * distinta, eso iría en el controlador que vamos a implementar en cada
- * microservicio y que a su vez herede de esta clase.
+ * El método editar no lo colocamos ya que cada Entity tiene una manera de
+ * actualizar distinta, eso iría en el controlador que vamos a implementar en
+ * cada microservicio y que a su vez herede de esta clase.
  * 
+ * Forma 01 de implementar CrossOrigin
+ * *********************************************************************
+ * 
+ * @CrossOrigin, es una de las maneras de permitir que se hagan peticiones de un
+ * dominio determinado. En nuestro proyecto, permitimos que se hagan peticiones
+ * desde localhost:4200, lugar donde está nuestra app de angular. Ahora, si se
+ * hacen peticiones desde un dominio que no hemos definido aquí, lanzará un
+ * error, pues el domino será bloqueado por la política de CORS.
  */
-
+@CrossOrigin({ "http://localhost:4200" })
 public class CommonController<E, S extends ICommonService<E>> {
 
 	protected final S service;
@@ -37,14 +46,12 @@ public class CommonController<E, S extends ICommonService<E>> {
 	public ResponseEntity<?> listar() {
 		return ResponseEntity.ok(this.service.findAll());
 	}
-	
+
 	/**
-	 * Tenemos que enviar dos parámetros desde el frontend
-	 * page: 0, 1, 2....etc (página actual)
-	 * size: Tamaño de la paginación. Ejm. 5 por página, 10 por página, etc.
-	 * Estos parámetros (page y size), aquí en el backend, son pasados 
-	 * por debajo, al objeto pageable y vinen desde el frontend vía url, 
-	 * ejmp: 
+	 * Tenemos que enviar dos parámetros desde el frontend page: 0, 1, 2....etc
+	 * (página actual) size: Tamaño de la paginación. Ejm. 5 por página, 10 por
+	 * página, etc. Estos parámetros (page y size), aquí en el backend, son pasados
+	 * por debajo, al objeto pageable y vinen desde el frontend vía url, ejmp:
 	 * http://localhost:8090/api/alumnos/pagina?page=0&size=7
 	 */
 	@GetMapping(path = "/pagina")
@@ -64,7 +71,7 @@ public class CommonController<E, S extends ICommonService<E>> {
 
 	@PostMapping
 	public ResponseEntity<?> crear(@Valid @RequestBody E entity, BindingResult result) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return this.validar(result);
 		}
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(entity));
